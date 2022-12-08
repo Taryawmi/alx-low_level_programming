4f9935955598daa437de1595c4361e3c200183e6
Taryawmi <teepee4uall@gmail.com>
2022-12-08 04:16:53 -0800

    sweet

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <studio.h>
 
 /**
-* main - prints the phrase "Programming is like building a multilingual puzzle
+* main - prints the phase "Programming is like building a multilingual puzzle
 * Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
