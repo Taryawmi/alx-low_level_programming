7075335beaf12c1ad280f4e154fee9e261bad391
Taryawmi <teepee4uall@gmail.com>
2022-12-14 01:34:23 -0800

    divine

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,24 +18,21 @@ k = j * i;
 if (j == 0)
 {
 _putchar(k + '0');
-}
-else if (k < 10 && j != 0)
+}else if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-}
-else if (k >= 10 && k < 100)
+}else if (k >= 10 && k < 100)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-}
-else if (k >= 100)
+}else if (k >= 100)
 {
 _putchar(',');
 _putchar(' ');
@@ -45,5 +42,6 @@ _putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
+}
 }
 }
